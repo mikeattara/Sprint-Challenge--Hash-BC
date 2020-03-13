8064f9ade3a97ccdace7440f385c601e99a27746
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-03-13 08:09:56 +0000

    clean up unused codeC:/Users/HP/.virtualenvs/Sprint-Challenge--Hash-BC-RvAWOA5M/Scripts/python.exe c:/Users/HP/Projects/Lambda/Sprint-Challenge--Hash-BC/hashtables/ex1/ex1_tests.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve,)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -29,8 +27,3 @@ def print_answer(answer):
         print(str(answer[0]) + " " + str(answer[1]))
     else:
         print("None")
-
-
-print_answer(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
-print_answer(get_indices_of_item_weights(
-    [12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7))
