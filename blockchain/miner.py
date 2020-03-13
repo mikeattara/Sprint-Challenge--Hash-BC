57a64c3adb5a497b8d859a5177ccd24d287811d7
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-03-13 10:05:37 +0000

    complete blockchain

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,7 +24,8 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof = random.getrandbits(32)
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,9 +39,10 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
+    hash = hashlib.sha256(str(proof).encode()).hexdigest()
+    last_hash = str(last_hash)
 
-    # TODO: Your code here!
-    pass
+    return last_hash[-6:] == hash[:6]
 
 
 if __name__ == '__main__':
