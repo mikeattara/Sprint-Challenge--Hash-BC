803f436bfdb8fafdb33825116e93b5ad505ea7b9
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-03-13 08:08:20 +0000

    complete ht ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,17 +7,30 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length)
 
-    """
-    YOUR CODE HERE
-    """
+    for index, weight in enumerate(weights):
+        hash_table_insert(ht, weight, index)
+
+    for index, weight in enumerate(weights):
+        key = limit - weight
+        entry = hash_table_retrieve(ht, key)
+        if entry is not None:
+            if index > entry:
+                return index, entry
+            else:
+                return entry, index
 
     return None
 
 
 def print_answer(answer):
     if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
+        print(str(answer[0]) + " " + str(answer[1]))
     else:
         print("None")
+
+
+print_answer(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
+print_answer(get_indices_of_item_weights(
+    [12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7))
